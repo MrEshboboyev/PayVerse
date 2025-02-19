5bc365fc80081f910e0d7953ff9dc79be38f0c5c
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-19 00:46:02 -0800

    Applied Proxy pattern to [Wallet];

---
+++ src/PayVerse.Infrastructure/Proxies/IWalletTransaction.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PayVerse.Domain.Entities.Wallets;
+using static PayVerse.Domain.Errors.DomainErrors;
+using static QuestPDF.Helpers.Colors;
+
+namespace PayVerse.Infrastructure.Proxies;
+
+// ✅ Benefits:
+// Ensures only the owner can execute wallet transactions.
+// Adds an extra security layer without modifying the core transaction logic.
+
+public interface IWalletTransaction
+{
+    void Execute(WalletTransaction transaction);
+}
---
+++ src/PayVerse.Infrastructure/Proxies/WalletTransactionProxy.cs
@@ -0,0 +1,19 @@
+﻿using PayVerse.Domain.Entities.Wallets;
+
+namespace PayVerse.Infrastructure.Proxies;
+
+public class WalletTransactionProxy(
+    IWalletTransaction walletTransaction, 
+    Guid walletId) : IWalletTransaction
+{
+    public void Execute(WalletTransaction transaction)
+    {
+        if (transaction.WalletId != walletId)
+        {
+            Console.WriteLine("Unauthorized transaction attempt.");
+            return;
+        }
+
+        walletTransaction.Execute(transaction);
+    }
+}
