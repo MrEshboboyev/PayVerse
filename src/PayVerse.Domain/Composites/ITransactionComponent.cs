ec1aff0bdba929350f01c2bc2a0dd6250b01bbde
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-19 00:41:43 -0800

    Applied Composite Pattern for [Transaction];

---
+++ src/PayVerse.Domain/Composites/ITransactionComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace PayVerse.Domain.Composites;
+
+// ✅ Benefits:
+// Enables batch processing of transactions.
+// Reduces overhead when processing large numbers of transactions.
+
+public interface ITransactionComponent
+{
+    void Process();
+}
---
+++ src/PayVerse.Domain/Composites/TransactionLeaf.cs
@@ -0,0 +1,9 @@
+﻿namespace PayVerse.Domain.Composites;
+
+public class TransactionLeaf(string transactionId, decimal amount) : ITransactionComponent
+{
+    public void Process()
+    {
+        Console.WriteLine($"Processing single transaction {transactionId} of ${amount}");
+    }
+}
