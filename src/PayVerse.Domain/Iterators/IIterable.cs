bc00bed769df3423d20499c1b42a63102d11722b
MrEshboboyev <mreshboboyev@gmail.com>
2025-03-06 14:49:32 +0500

    Iterator pattern applied : generic interfaces (IIterable, IIterator); Remove old related files

---
+++ src/PayVerse.Domain/Iterators/IIterable.cs
@@ -0,0 +1,10 @@
+﻿namespace PayVerse.Domain.Iterators;
+
+/// <summary>
+/// Interface for collections that can be iterated
+/// </summary>
+/// <typeparam name="T">Type of elements in the collection</typeparam>
+public interface IIterable<T>
+{
+    IIterator<T> CreateIterator();
+}
--- src/PayVerse.Domain/Iterators/IIterator.cs
+++ src/PayVerse.Domain/Iterators/IIterator.cs
@@ -1,9 +1,14 @@
 ﻿namespace PayVerse.Domain.Iterators;
 
-// ✅ Supports pagination without exposing internal transaction details.
 
+/// <summary>
+/// Generic interface for Iterator pattern
+/// </summary>
+/// <typeparam name="T">Type of elements to iterate</typeparam>
 public interface IIterator<T>
 {
     bool HasNext();
     T Next();
+    void Reset();
+    T Current();
 }
